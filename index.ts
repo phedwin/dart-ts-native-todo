9e9ae84dbc5abd222beb71055c2d22698c42679e
phedwin <engineer@realmelevators.com>
2024-02-01 11:22:30 +0300

    track node_modules

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-import { Details } from "./lib/src/components/Interface/DetailsType";
-import { todos } from "./lib/src/components/addTodo";
 
 
-let c =  new todos("first assignment" as string, true as boolean,
-{
-    year : 2004,
-    text : ["did evenythin i was told",
-            "ended up not seeiing anythin cool",
-            "this sucks soo hard"
-        ],
-    author : "Jared"
-} as Details).showTodos();
+console.log(cacheType);
