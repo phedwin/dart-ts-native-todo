e313c59a2ca59d2232efc2120f8e428ccb101816
phedwin <engineer@realmelevators.com>
2024-02-11 23:57:58 +0300

    make user factories and seed to prisma

--- lib/Database/factories/UserFactory.ts
+++ lib/Database/factories/UserFactory.ts
@@ -1,3 +1,22 @@
-
-
-
+class UserFactory {
+    private static usernames = ['john_doe', 'jane_smith', 'sam_wilson'];
+    private static domains = ['example.com', 'test.com', 'example.org'];
+  
+    static generateRandomUsername(): string {
+      const randomIndex = Math.floor(Math.random() * UserFactory.usernames.length);
+      return UserFactory.usernames[randomIndex];
+    }
+  
+    static generateRandomEmail(): string {
+      const username = this.generateRandomUsername();
+      const domain = this.domains[Math.floor(Math.random() * this.domains.length)];
+      return `${username}@${domain}`;
+    }
+  
+    static generateRandomPassword(): string {
+      return Math.random().toString(36).slice(2); // Generates a random alphanumeric string
+    }
+  }
+  
+  export default UserFactory;
+  
--- lib/Database/seeders/seeder.ts
+++ lib/Database/seeders/seeder.ts
@@ -1,2 +1,33 @@
+
 import { prisma } from "../client";
+import UserFactory from "../factories/UserFactory";
+
+const numberOfUsers = 10;
+
+const users: { username: string; email: string; password: string; }[] = [];
+
+for (let i = 0; i < numberOfUsers; i++) {
+  const username = UserFactory.generateRandomUsername();
+  const email = UserFactory.generateRandomEmail();
+  const password = UserFactory.generateRandomPassword();
+  users.push({ username, email , password});
+}
+
+
+async function main() {
+    const user = prisma.users.createMany({ data : users})
+  }
+  
+main()
+
+.then(async () => {
+    await prisma.$disconnect()
+})
+.catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
+
+
 
